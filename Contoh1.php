16318e4b1a24f000cc4b1d6e6ec0d464174a904b
WP2-NabilFahriRahmatullah <90917399+WP2-NabilFahriRahmatullah@users.noreply.github.com>
2021-09-18 21:04:14 +0700

    Add files via upload

---
+++ Contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controllers
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo"Nama sayan Nabil Fahri Rahmatullah
+            Saya tinggal di daerah Citeureup
+            olahraga yang saya sukai adalah
+            Bulutangkis";
+    }
+}
